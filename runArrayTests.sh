c71f281c02d96eb80e303ccec6b3d1bcbe0ceabc
yuri_ogorodnikov <yogorodnikov@hidevlab.com>
2012-08-22 15:47:12 +0700

    yura: deleted arrayEach and so on

--- runArrayTests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -jar JsTestDriver-1.3.4.b.jar --testOutput test-arr-output/ --tests arrayTest --browser /usr/lib/firefox/firefox --port 4242
-cd test-arr-output
-genhtml jsTestDriver.conf-coverage.dat
