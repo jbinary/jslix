40f4119514dff5d6019619e61f028f2b9968f256
Comrade DOS <dos@hidevlab.com>
2012-11-13 01:20:12 +0700

    Implementation of PLAIN authentication mechanism for jslix.sasl module.

---
+++ src/jslix.sasl.mechanisms.plain.js
@@ -0,0 +1,23 @@
+(function(){
+    
+    var jslix = window.jslix;
+
+    jslix.sasl.mechanisms['PLAIN'] = function(dispatcher){
+        if(jslix.sasl === undefined)
+            throw Error('Load sasl plugin first.');
+        this._dispatcher = dispatcher;
+    };
+
+    jslix.sasl.mechanisms['PLAIN'].prototype.getPlainMessage = function(){
+        // TODO: Replace with CryptoJS
+        return b64encode(this._dispatcher.connection.jid.getBareJID() + '\0' + this._dispatcher.connection.jid.getNode() + '\0' + this._dispatcher.connection.password);
+    }
+
+    jslix.sasl.mechanisms['PLAIN'].prototype.auth = function(){
+        return jslix.sasl.stanzas.auth.create({
+            mechanism: 'PLAIN',
+            content: this.getPlainMessage()
+        });
+    }
+
+})();
