ea5c37a391c483f45fabdff699dfd2c36df66f9f
Comrade DOS <dos@hidevlab.com>
2012-11-13 00:26:36 +0700

    Added method for reset connection.

--- src/jslix.connection.js
+++ src/jslix.connection.js
@@ -8,10 +8,17 @@
         this._connection = window.con;
     }
 
+    jslix.connection.prototype.reInitStream = function(){
+        if(!this._connection)
+            return false;
+        return this._connection._reInitStream(JSJaC.bind(
+            this._connection._doStreamBind, this._connection));
+    }
+
     jslix.connection.prototype.sendRaw = function(doc){
         if(!this._connection)
             return false;
-        var packet = new window.JSJaCPacket('');
+        var packet = new JSJaCPacket('');
         packet.doc = doc;
         return this._connection.send(packet);
     }
