3b549a65c4bc1f3a1801246bb266f471b5a653b7
Comrade DOS <dos@hidevlab.com>
2012-11-12 20:27:37 +0700

    Add some wrapper functions for JSJaCConnection.

--- src/jslix.connection.js
+++ src/jslix.connection.js
@@ -1,7 +1,25 @@
 (function(){
+
     var jslix = window.jslix;
+
     jslix.connection = function(jid, password){
         this.jid = new jslix.JID(jid);
         this.password = password;
+        this._connection = window.con;
     }
+
+    jslix.connection.prototype.sendRaw = function(doc){
+        if(!this._connection)
+            return false;
+        var packet = new window.JSJaCPacket('');
+        packet.doc = doc;
+        return this._connection.send(packet);
+    }
+
+    jslix.connection.prototype.disconnect = function(){
+        if(!this._connection)
+            return false;
+        return this._connection.disconnect();
+    }
+
 })();
