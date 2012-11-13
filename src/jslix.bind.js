7a5b6c62d62a0c7a008556b718fb5eec57e5a19d
Comrade DOS <dos@hidevlab.com>
2012-11-13 20:39:23 +0700

    Add jslix.bind module.

---
+++ src/jslix.bind.js
@@ -0,0 +1,31 @@
+(function(){
+
+    var jslix = window.jslix;
+
+    jslix.bind = function(dispatcher){
+        this._dispatcher = dispatcher;
+        this._dispatcher.addTopHandler(jslix.bind.stanzas.response, this);
+    }
+
+    jslix.bind.NS_BIND = 'urn:ietf:params:xml:ns:xmpp-bind';
+
+    jslix.bind.stanzas = {};
+
+    jslix.bind.stanzas.base = jslix.Element({
+        xmlns: jslix.bind.NS_BIND,
+        element_name: 'bind',
+        parent_element: jslix.stanzas.iq
+    });
+
+    jslix.bind.stanzas.request = jslix.Element({
+        resource: new jslix.fields.StringNode('resource', true)
+    }, [jslix.bind.stanzas.base]);
+
+    jslix.bind.stanzas.response = jslix.Element({
+        jid: new jslix.fields.StringNode('jid', true),
+        handler: function(result, top){
+            this._dispatcher.connection.jid = new jslix.JID(result.jid);
+        }
+    }, [jslix.bind.stanzas.base]);
+
+})();
