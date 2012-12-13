fb0e3973e51f6d35aaefab167a30967f056f0e34
Comrade DOS <dos@hidevlab.com>
2012-12-13 16:38:43 +0700

    Refactor jslix.version module.

--- src/jslix.version.js
+++ src/jslix.version.js
@@ -1,17 +1,14 @@
 "use strict";
 (function(window) {
 
-    var jslix = window.jslix;
-    var fields = jslix.fields;
+    var jslix = window.jslix,
+        fields = jslix.fields;
 
     jslix.version = function(dispatcher) {
         this._name = '';
         this._version = '';
-        this._os = '';
-
-        this._dispatcher = dispatcher;
-
         this._os = jslix.version._defineOs();
+        this._dispatcher = dispatcher;
 
     };
 
@@ -42,27 +39,26 @@
     }
 
     jslix.version._defineOs = function(){
-        var os = window.navigator ? window.navigator.appName : 'n/a';
-        var version = window.navigator ? window.navigator.appVersion: 'n/a';
+        var os = window.navigator ? window.navigator.appName : 'n/a',
+            version = window.navigator ? window.navigator.appVersion: 'n/a';
         return version != 'n/a' ? os + ' ' + version : os;
     };
 
     jslix.version.prototype.get = function(jid) {
-        var query = jslix.version.stanzas.request.create();
-        var iq = jslix.stanzas.iq.create(
-                    {
-                        type: 'get',
-                        to: jid,
-                        link: query
-                    });
-        return this._dispatcher.send(query);
+        var iq = jslix.stanzas.iq.create({
+            type: 'get',
+            to: jid,
+            link: jslix.version.stanzas.request.create()
+        });
+        return this._dispatcher.send(iq);
     };
 
     jslix.version.prototype.init = function(name, version){
         this.setName(name);
         this.setVersion(version);
-
-        if (this._dispatcher) this._dispatcher.addHandler(jslix.version.stanzas.request, this);
+        if (this._dispatcher){
+            this._dispatcher.addHandler(jslix.version.stanzas.request, this);
+        }
     };
 
     jslix.version.stanzas.response = jslix.Element({
@@ -80,14 +76,11 @@
         //Handlers
         result_class: jslix.version.stanzas.response,
         getHandler: function(query, top) {
-            var result = query.makeResult(
-                {
-                    version: this.getVersion(),
-                    name: this.getName(),
-                    os:  this.getOs()
-                }
-            );
-            return result;
+            return query.makeResult({
+                version: this.getVersion(),
+                name: this.getName(),
+                os:  this.getOs()
+            });
         }
     }, [jslix.stanzas.query]);
 
