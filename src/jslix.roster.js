19fd2f3ac4aa75dbf2b004cf93bdd671f8c887f0
Binary <binary@jrudevels.org>
2013-01-30 15:57:54 +0700

    initial roster support

--- src/jslix.roster.js
+++ src/jslix.roster.js
@@ -1,20 +1,69 @@
+/*
+ * JSLix Roster Implementation.
+ * Requires the js-signals library
+ *
+ */
+
 "use strict";
 (function(){
 
     var jslix = window.jslix;
+    var fields = jslix.fields;
+    var Signal = signals.Signal;
 
     jslix.roster = function(dispatcher){
         this._dispatcher = dispatcher;
+        this.signals = roster.signals;
+    }
+    var roster = jslix.roster;
+
+    roster._name = 'jslix.roster';
+
+    roster.ROSTER_NS = 'jabber:iq:roster';
+
+    // Signals
+    roster.signals = {
+        got: new Signal()
     }
 
-    jslix.roster._name = 'jslix.roster';
+    // Stanzas
+    roster.stanzas = {};
+    var stanzas = roster.stanzas;
 
-    jslix.roster.ROSTER_NS = 'jabber:iq:roster';
+    stanzas.item = jslix.Element({
+        element_name: 'item',
+        xmlns: roster.ROSTER_NS,
 
-    jslix.roster.stanzas = {};
+        jid: new fields.JIDAttr('jid'),
+        subscription: new fields.StringAttr('subscription', false),
+        ask: new fields.StringAttr('ask', false),
+        nick: new fields.StringAttr('name', false),
+        groups: new fields.StringNode('group', false, true)
+    });
 
-    jslix.roster.stanzas.request = jslix.Element({
-        xmlns: jslix.roster.ROSTER_NS
+    stanzas.response = jslix.Element({
+        xmlns: roster.ROSTER_NS,
+        items: new fields.ElementNode(stanzas.item, false, true)
     }, [jslix.stanzas.query]);
 
+    stanzas.request = jslix.Element({
+        result_class: stanzas.response,
+        xmlns: roster.ROSTER_NS
+    }, [jslix.stanzas.query]);
+
+    // Methods
+    roster.prototype.init = function() {
+        var d = $.Deferred();
+        var request = stanzas.request.create({
+            parent: jslix.stanzas.iq.create({type: 'get'})
+        });
+        var that = this;
+        this._dispatcher.send(request).done(function(result) {
+            roster.signals.got.dispatch(result);
+            d.resolve(result);
+        }).fail(function(failure) {
+            d.reject(failure);
+        });
+        return d;
+    }
 })();
