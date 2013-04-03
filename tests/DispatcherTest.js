b41e04c8e930f530c36586e725b92f8433f5b020
Comrade DOS <dos@hidevlab.com>
2013-04-03 20:51:21 +0700

    Add test for addHook function.

--- tests/DispatcherTest.js
+++ tests/DispatcherTest.js
@@ -9,5 +9,13 @@ var DispatcherTest = buster.testCase('DispatcherTest', {
         this.dispatcher.unregisterPlugin(plugin);
         refute(plugin._name in this.dispatcher.plugins);
         refute(this.dispatcher.handlers.lenght && this.dispatcher.top_handlers.lenght);
+    },
+    testAddHook: function(){
+        assert(this.dispatcher.hooks['stub'] == undefined);
+        this.dispatcher.addHook('stub', function(){
+            return stub;
+        }, {stub: 'stub'}, 'stub');
+        assert(this.dispatcher.hooks['stub'] instanceof Array);
+        assert(this.dispatcher.hooks['stub'].length == 1);
     }
 });
