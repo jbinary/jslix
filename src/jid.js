deed6a3f8b1f1577366080697e09f53f23d8ec70
Comrade DOS <dos@hidevlab.com>
2013-05-30 15:34:54 +0700

    Small refactoring of jid module.

--- src/jid.js
+++ src/jid.js
@@ -1,46 +1,40 @@
 "use strict";
 define(['jslix/class', 'jslix/exceptions'], function(Class, exceptions){
 
-    var JID_FORBIDDEN = ['"',' ','&','\'','/',':','<','>','@'];
+    var JID_FORBIDDEN = ['"',' ','&','\'','/',':','<','>','@'],
+        codesForEscape = {
+            ' ' : '20',
+            '"' : '22',
+            '&' : '26',
+            '\'' : '27',
+            '/' : '2f',
+            ':' : '3a',
+            '<' : '3c',
+            '>' : '3e',
+            '@' : '40',
+            '\\' : '5c'
+        },
+        codesForUnescape = {};
 
-    var codesForEscape = {
-        ' ' : '20',
-        '"' : '22',
-        '&' : '26',
-        '\'' : '27',
-        '/' : '2f',
-        ':' : '3a',
-        '<' : '3c',
-        '>' : '3e',
-        '@' : '40',
-        '\\' : '5c'
-    };
-
-    var codesForUnescape = {};
-
-    for(var key in codesForEscape)
-      codesForUnescape[codesForEscape[key]] = key;
+    for(var key in codesForEscape){
+        codesForUnescape[codesForEscape[key]] = key;
+    }
 
     var JID = function(jid){
         if (typeof(jid) == 'string'){
-           if (jid.indexOf('@') != -1){
-              this.node = jid.substring(0, jid.indexOf('@'));
-              jid = jid.substring(jid.indexOf('@') + 1);
-           }else{
-                this.node = '';
-           }
-
-          if (jid.indexOf('/') != -1){
-             this.resource = jid.substring(jid.indexOf('/') + 1);
-             jid = jid.substring(0, jid.indexOf('/'));
-          }else{
-             this.resource = '';
-          }
-          this.domain = jid;
+            if (jid.indexOf('@') != -1){
+                this.node = jid.substring(0, jid.indexOf('@'));
+                jid = jid.substring(jid.indexOf('@') + 1);
+            }
+            if (jid.indexOf('/') != -1){
+               this.resource = jid.substring(jid.indexOf('/') + 1);
+               jid = jid.substring(0, jid.indexOf('/'));
+            }
+            this.domain = jid;
         }else{
-           this.node = jid.node;
-           this.domain = jid.domain;
-           this.resource = jid.resource;
+            this.node = jid.node;
+            this.domain = jid.domain;
+            this.resource = jid.resource;
         }
     };
 
@@ -76,17 +70,22 @@ define(['jslix/class', 'jslix/exceptions'], function(Class, exceptions){
             this._resource = new_resource;
         },
         get bare(){
-            return this.node + '@' + this.domain;
+            if(this.node){
+                return this.node + '@' + this.domain;
+            }
+            return this.domain;
         }
     }
 
     JID.prototype.toString = function(){
         var jid = '';
-        if (this.node)
-            jid = this.node + '@';
-        jid += this.domain;
-        if (this.resource)
+        if (this.node){
+            jid += this.node + '@';
+        }
+        jid += this.domain || '';
+        if (this.resource){
             jid += '/' + this.resource;
+        }
         return jid;
     };
 
@@ -98,27 +97,26 @@ define(['jslix/class', 'jslix/exceptions'], function(Class, exceptions){
     };
 
     JID.prototype.isEntity = function(jid){
-        var jid =  jid instanceof JID ? jid.clone(true) : new JID(jid).clone(true),
+        var jid = jid instanceof JID ? jid.clone(true) : new JID(jid).clone(true),
             clone = this.clone(true);
-        return (clone.toString() === jid.toString());
+        return clone.toString() === jid.toString();
     };
 
     JID.prototype.escape = function(node, domain, resource){
         var escapeNode = '';
-        for (var i = 0; i < node.length; i++)
-            if (JID_FORBIDDEN.indexOf(node[i]) != -1)
+        for (var i = 0; i < node.length; i++){
+            if (JID_FORBIDDEN.indexOf(node[i]) != -1){
                 escapeNode += '\\' + codesForEscape[node[i]];
-        else{
-            //if situation like c:\5ccommon
-            if (i < node.length - 2 && node[i] == '\\'){
-                var code = node.slice(i + 1, i + 3);
-                var key = codesForUnescape[code];
-                if (key){
-                    escapeNode += '\\' + codesForEscape[key];
-                }else
+            }else{
+                //if situation like c:\5ccommon
+                if (i < node.length - 2 && node[i] == '\\'){
+                    var code = node.slice(i + 1, i + 3);
+                    var key = codesForUnescape[code];
+                    escapeNode += key ? '\\' + codesForEscape[key] : node[i];
+                }else{
                     escapeNode += node[i];
-            }else
-                escapeNode += node[i];
+                }
+            }
         }
         return new JID({
             node: escapeNode,
@@ -129,32 +127,37 @@ define(['jslix/class', 'jslix/exceptions'], function(Class, exceptions){
 
     JID.prototype.unescape = function(){
         var resultJID = '',
-            node = this.node;
-        for(var i=0; i < node.length; i++){
+            node = this.node || '';
+        for(var i=0; i<node.length; i++){
             if (JID_FORBIDDEN.indexOf(node[i]) != -1 && node[i] != '\\')
                 throw new JIDInvalidException("forbidden char in escape nodename: " + JID_FORBIDDEN[i]);
             if (node[i] == '\\'){
                 var code = node.slice(i + 1, i + 3);
                 var key = codesForUnescape[code];
                 if (key){
-                    if (key == ' ' && (i == 0 || i == node.length - 3))
+                    if (key == ' ' && (i == 0 || i == node.length - 3)){
                         throw new JIDInvalidException("wrong unescape: space at the beginning or at the end");
+                    }
                     resultJID += key;
                     i += 2;
-                }else
+                }else{
                     resultJID += node[i];
-            }else
+                }
+            }else{
                 resultJID += node[i];
+            }
         }
         resultJID += '@' + this.domain;
-        if (this.resource)
+        if (this.resource){
             resultJID += '/' + this.resource;
+        }
         return resultJID;
     };
 
     JID._checkNodeName = function(nodeprep){
-        if (!nodeprep || nodeprep == '')
+        if (!nodeprep){
             return;
+        }
         for (var i=0; i< JID_FORBIDDEN.length; i++)
             if (nodeprep.indexOf(JID_FORBIDDEN[i]) != -1)
                 throw new JIDInvalidException("forbidden char in nodename: " + JID_FORBIDDEN[i]);
