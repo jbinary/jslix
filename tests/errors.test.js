e733208cb0587cd312880521b47f62e4f80e3c53
Binary <binary@jrudevels.org>
2014-01-06 19:28:49 +0700

    fix tests

---
+++ tests/errors.test.js
@@ -0,0 +1,15 @@
+define(['jslix/common', 'jslix/errors', 'jslix/stanzas'],
+    function(jslix, errors, stanzas) {
+    buster.testCase('JslixTest', {
+        testErrorStanza: function(){
+            var error_stanza = errors.ErrorStanza.create({type: 'some_wrong_type'});
+            assert(error_stanza.type == 'some_wrong_type');
+            error_stanza = jslix.build(stanzas.MessageStanza.create({
+                link: error_stanza
+            }));
+            assert.exception(function(){
+                jslix.parse(error_stanza, errors.ErrorStanza);
+            }, 'ElementParseError');
+        }
+    });
+});
